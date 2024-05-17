6ae57eaed6154eed52ba31041f9776fee21fa408
Raghuveer <raghuveer120203@gmail.com>
2024-05-17 06:19:09 +0530

    deployedWebsite

---
+++ src/Deploy.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Deploy = () => {
+  return (
+    <div style={{ marginTop: 50 }}>
+      <h1 style={{ color: "green" }}>
+        This app id deployed on github repository
+      </h1>
+    </div>
+  );
+};
+
+export default Deploy;
